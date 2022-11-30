39f26b92eb1cb22589446ab88aa5a1480dfcdbbb
tienyasuo729 <tienngqueson@gmail.com>
2022-12-01 00:48:38 +0700

    codegym

--- module2/ss5_Access modifier_ static method_static property/bai_tap/Access_modifier/Circle.java
+++ module2/ss5_Access modifier_ static method_static property/bai_tap/Access_modifier/Circle.java
@@ -1,38 +1,19 @@
-package test;
-
 public class Circle {
-    int radius;
-    String color;
+    private double radius = 1.0;
+    private String color = "red";
 
     public Circle() {
     }
 
-    public Circle(int radius, String color) {
+    public Circle(double radius) {
         this.radius = radius;
-        this.color = color;
     }
 
-    public int getRadius() {
+    private double getRadius() {
         return radius;
     }
 
-    public void setRadius(int radius) {
-        this.radius = radius;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    @Override
-    public String toString() {
-        return "Circle{" +
-                "radius=" + radius +
-                ", color='" + color + '\'' +
-                '}';
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
     }
 }
