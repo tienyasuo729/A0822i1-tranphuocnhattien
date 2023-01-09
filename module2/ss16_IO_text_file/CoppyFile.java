17e22c18aa4de4459761ea45339b5edc50f190ec
tienyasuo729 <tienngqueson@gmail.com>
2023-01-09 21:23:56 +0700

    codegym

--- module2/ss16_IO_text_file/CoppyFile.java
+++ module2/ss16_IO_text_file/CoppyFile.java
@@ -1,25 +1,36 @@
+import javafx.scene.image.PixelFormat;
+
 import java.io.*;
+import java.lang.reflect.Array;
 
 public class CoppyFile {
 
     public static void main(String[] args) {
         try {
-            FileWriter fileWriter = new FileWriter("D:\\codegym\\A0822i1-tranphuocnhattien\\Module2\\ss16_IO_text_file\\destination");
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            FileWriter fileWriterSource = new FileWriter("C:\\codegym\\A0822i1-tranphuocnhattien\\module2\\ss16_IO_text_file\\source");
+            BufferedWriter bufferedWriterSource = new BufferedWriter(fileWriterSource);
             String a = "hello";
-            bufferedWriter.write(a);
-            bufferedWriter.newLine();
+            bufferedWriterSource.write(a);
+            bufferedWriterSource.newLine();
             String b = "world";
-            bufferedWriter.write(b);
-            bufferedWriter.close();
-            fileWriter.close();
-            FileReader fileReader =new FileReader("D:\\codegym\\A0822i1-tranphuocnhattien\\Module2\\ss16_IO_text_file\\destination");
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String line = null;
-            while ((line = bufferedReader.readLine()) != null){
-                line = bufferedReader.readLine();
-                System.out.println(line);
+            bufferedWriterSource.write(b);
+            bufferedWriterSource.close();
+            fileWriterSource.close();
+            FileReader fileReaderSource =new FileReader("C:\\codegym\\A0822i1-tranphuocnhattien\\module2\\ss16_IO_text_file\\source");
+            BufferedReader bufferedReaderSource = new BufferedReader(fileReaderSource);
+
+            FileWriter fileWriterDestination = new FileWriter("C:\\codegym\\A0822i1-tranphuocnhattien\\module2\\ss16_IO_text_file\\destination");
+            BufferedWriter bufferedWriterDestination = new BufferedWriter(fileWriterDestination);
+            String aff = null;
+            while (( aff = bufferedReaderSource.readLine()) != null){
+                System.out.println(aff);
+                bufferedWriterDestination.write(bufferedReaderSource.readLine());
+                bufferedWriterDestination.newLine();
             }
+            fileWriterDestination.close();
+            bufferedWriterDestination.close();
+            fileReaderSource.close();
+            bufferedReaderSource.close();
         }catch (FileNotFoundException e){
             e.getStackTrace();
         }catch (Exception e){
